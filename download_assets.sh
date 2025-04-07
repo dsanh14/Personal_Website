99059f77672298a66670f61af3d7dadf17090d6d
dsanh14 <dsanh999@gmail.com>
2025-04-07 14:03:03 -0700

    new file:   Assets/arrow.png 	new file:   Assets/checkmark.png 	new file:   Assets/education.png 	new file:   Assets/email.png 	new file:   Assets/experience.png 	new file:   Assets/github.png 	new file:   Assets/linkedin.png 	new file:   Assets/trophy.png 	new file:   download_assets.sh 	modified:   index.html

---
+++ download_assets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd Assets
+
+# Download icons from Icons8
+curl -o experience.png https://img.icons8.com/ios-filled/50/000000/briefcase.png
+curl -o education.png https://img.icons8.com/ios-filled/50/000000/graduation-cap.png
+curl -o checkmark.png https://img.icons8.com/ios-filled/50/000000/checkmark.png
+curl -o trophy.png https://img.icons8.com/ios-filled/50/000000/trophy.png
+curl -o linkedin.png https://img.icons8.com/ios-filled/50/000000/linkedin.png
+curl -o github.png https://img.icons8.com/ios-filled/50/000000/github.png
+curl -o email.png https://img.icons8.com/ios-filled/50/000000/email.png
+curl -o arrow.png https://img.icons8.com/ios-filled/50/000000/down-arrow.png
+
+echo "Icons downloaded successfully!" 
